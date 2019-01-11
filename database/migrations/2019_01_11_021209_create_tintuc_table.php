4118d1f880d9f0357fa17b27e5fd30219454beec
chiendv6948 <chiendv6948@co-well.com.vn>
2019-01-11 09:49:24 +0700

    migrate

---
+++ database/migrations/2019_01_11_021209_create_tintuc_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTintucTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tintuc', function (Blueprint $table) {
+            $table->increments('id');
+            $table->increments('tl_id');
+            $table->string('title');
+            $table->string('slug');
+            $table->text('content');
+            $table->string('image');
+            $table->string('status');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tintuc');
+    }
+}
